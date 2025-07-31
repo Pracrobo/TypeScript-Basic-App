19083aaf8b668b049bf0f77007f6ac9e97d1e049
Pracrobo <cpu180621@gmail.com>
2025-07-31 11:19:46 +0900

    fix: 직접 구현한 부분 위쪽으로 옮김

--- basic/09.UtilityTypes/src/chapter1.ts
+++ basic/09.UtilityTypes/src/chapter1.ts
@@ -7,6 +7,12 @@
  * 특정 객체 타입의 모든 프로퍼티를 선택적 프로퍼티로 바꿔주는 타입
  *
  */
+// 직접 구현
+type Partial<T> = {
+  // keyof T : union
+  [key in keyof T]?: T[key]; //indexed Access Type
+  // key in keyof T : Mapped Type : key in a|b|c|
+};
 
 interface Post {
   title: string;
@@ -14,12 +20,6 @@ interface Post {
   content: string;
   thumbnailURL?: string;
 }
-// 직접 구현
-type Partial<T> = {
-  // keyof T : union
-  [key in keyof T]?: T[key]; //indexed Access Type
-  // key in keyof T : Mapped Type : key in a|b|c|
-};
 const draft: Partial<Post> = {
   title: "제목 나중에 짓자",
   content: "초안..",
@@ -47,7 +47,7 @@ const withThumbnailPost: Required<Post> = {
  * Readonly<T>: 읽기 전용 수정 불가
  * 특정 객체 타입에서 모든 프로퍼티를 읽기 전용 프로퍼티로 만들어주는 타입
  */
-
+// 직접 구현
 type Readonly<T> = {
   readonly [key in keyof T]: T[key];
 };
--- basic/09.UtilityTypes/src/chapter2.ts
+++ basic/09.UtilityTypes/src/chapter2.ts
@@ -9,14 +9,7 @@
  * Pick<T, K> : 뽑다, 고르다
  * 객체 타입으로부터 특정 프로퍼티만 딱 골라내는 그런 타입
  */
-
-interface Post {
-  title: string;
-  tags: string[];
-  content: string;
-  thumbnailURL?: string;
-}
-
+// 직접 구현
 type Pick<T, K extends keyof T> = {
   // keyof T의 subtype만 들어올 수 있는 K가 된다.
   // K extends 'title' | 'tags' | 'content' | 'thumbnailURL'
@@ -24,6 +17,13 @@ type Pick<T, K extends keyof T> = {
   // True
   [key in K]: T[key]; //Type 'K' is not assignable to type 'string | number | symbol
 };
+
+interface Post {
+  title: string;
+  tags: string[];
+  content: string;
+  thumbnailURL?: string;
+}
 
 const legacyPost: Pick<Post, "title" | "content"> = {
   title: "옛글",
@@ -34,6 +34,7 @@ const legacyPost: Pick<Post, "title" | "content"> = {
  * Omit <T, K>  : 생략하다, 빼다
  * 객체 타입으로부터 특정 프로퍼티를 제거하는 타입
  */
+// 직접 구현
 type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 // T = Post, K = "title"
 // Pick <Post, Exclude <keyof Post, 'title'>>
@@ -52,6 +53,12 @@ const noTitlePost: Omit<Post, "title"> = {
  * Record<K, V>
  * 객체 타입을 정의할때 사용한다
  */
+// 직접 구현
+// K에 들어오는 타입은 어떤 객체의 타입의 키를 추출해놓은 유니온타입이다.
+// 무슨 객체인지는 모르겠지만 어떤 객체의 키타입이다
+type Record<K extends keyof any, V> = {
+  [key in K]: V;
+};
 
 // 증복이 많아 코드가 아쉽다
 type thumbnailLegacy = {
@@ -59,13 +66,6 @@ type thumbnailLegacy = {
   medium: { url: string };
   small: { url: string };
   watch: { url: string };
-};
-
-// 직접 구현
-// K에 들어오는 타입은 어떤 객체의 타입의 키를 추출해놓은 유니온타입이다.
-// 무슨 객체인지는 모르겠지만 어떤 객체의 키타입이다
-type Record<K extends keyof any, V> = {
-  [key in K]: V;
 };
 
 type Thumbnail = Record<
